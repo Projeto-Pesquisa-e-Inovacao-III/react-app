bbfbb18063bd0797b3cc00fb7c6f111b82f7697c
FillipeMendesC <fillipemcoelho@hotmail.com>
2025-08-28 16:37:23 -0300

    Revert "Feat/multipage"

--- src/routes/User/Login/index.tsx
+++ src/routes/User/Login/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import "./style.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Eye, EyeOff, Lock, Mail } from "lucide-react";
 import * as userService from "../../../constants/user";
-import Swal from "sweetalert2";
 
 export default function Login() {
   const [email, setEmail] = useState<string>("");
@@ -11,42 +10,16 @@ export default function Login() {
   const [remember, setRemember] = useState<boolean>(false);
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  const nav = useNavigate()
-
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     // Handle login logic here
     userService
       .login(email, password)
       .then((res) => {
-        console.log(res)
-        const isUserActive = res.data.ativo;
-        if (isUserActive) {
-          Swal.fire({
-            icon: "success",
-            title: "Login bem sucedido",
-            showConfirmButton: false,
-            timer: 3000,
-            allowOutsideClick: false,
-            allowEscapeKey: false
-          });
-          localStorage.setItem("user-info", JSON.stringify(res.data))
-          setTimeout(() => {
-            nav("/")
-          }, 3000)
-        } else {
-          return Promise.reject(new Error("Usuário inativo"))
-        }
+        console.log(res);
       })
       .catch((err) => {
-        Swal.fire({
-          icon: "error",
-          title: "Email/senha incorreto",
-          showConfirmButton: true,
-          confirmButtonColor: "#166ba3ff",
-          timer: 3000,
-        });
-        console.log(err)
+        console.error(err);
       });
   }
 
