47fbcb9f9e3c8a2ea646592bb066f4dfd1f026f9
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-10-17 19:55:41 -0300

    feat: add CalendarMini component and integrate into App routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,18 @@ import Logout from "./routes/User/Logout";
 import Calendar from "./components/Calendar";
 import CalendarWeek from "./components/CalendarWeek";
 import DevDebug from "./components/DevDebug";
+import CalendarMini from "./components/CalendarMini";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
         {/* todo: remove this  */}
-        <DevDebug />
+        {/* <DevDebug /> */}
         <Routes>
 
           <Route path="/" element={<Calendar />} />
+          <Route path="/mini" element={<CalendarMini  />} />
           <Route path="/week" element={<CalendarWeek />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
