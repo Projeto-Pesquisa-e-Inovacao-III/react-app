9745b643ef043d5b4934d78e627b1b63261c95b8
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-10-20 18:35:22 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 import "./style.css";
+import { Search } from "lucide-react";
+import InputWithIcon from "../AuthComponents/InputWithIcon";
 
+//todo: so here, we dont have any svg saved locally. might get 
 export function SearchBar() {
     const [search, setSearch] = useState("");
 
     return (
         <div className="search-bar">
-            <img src="/searchIcon.svg" alt="Pesquisar" className="search-icon" />
-            <input
-                type="text"
-                placeholder="Pesquisar por nome"
-                value={search}
-                onChange={e => setSearch(e.target.value)}
-                className="search-input"
-            />
+            <InputWithIcon type="text" placeholder="Pesquisar por nome" onInputChange={setSearch} icon={<Search />} />
         </div>  
     );
 }
