36e1850870f0c2cb0c737455242221883cdd0106
FillipeMendesC <fillipemcoelho@hotmail.com>
2025-10-20 12:13:56 -0300

    feat - Edit User page desktop and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./routes/User/Register";
-import EditUser from "./routes/EditUser";
+import EditUser from "./routes/EditUser/EditUser";
 import Logout from "./routes/User/Logout";
 import ViewSchedule from "./routes/Schedule";
 import Login from "./routes/User/Login";
 import ForgotPassword from "./routes/User/ForgotPassword";
+
+
 function App() {
 
 
   return (
-    <BrowserRouter>
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+      <BrowserRouter>
       <div className="container">
         <Routes>
           <Route path="/" element={<ViewSchedule />} />
@@ -24,6 +30,7 @@ function App() {
         </Routes>
       </div>
     </BrowserRouter>
+    </>
   );
 
 }
--- src/components/WhiteContainer/WhiteContainer.tsx
+++ src/components/WhiteContainer/WhiteContainer.tsx
@@ -1,16 +1,21 @@
 import React, { type ReactNode } from "react";
 import "./WhiteContainer.css"
 
-interface WhiteContainerProps {
+type WhiteContainerProps = {
     title?: string | "",
     children?: ReactNode,
-    gap?: number
+    gap?: number,
+    titleMarginBottom?: number
 }
 
-export function WhiteContainer({ title, children, gap }: WhiteContainerProps) {
+export function WhiteContainer({ title, children, gap, titleMarginBottom }: WhiteContainerProps) {
     return (
         <div className="white-container">
-            <h2>{title}</h2>
+            <h2
+            style={{
+                marginBottom: titleMarginBottom || 20
+            }}
+            >{title}</h2>
             <div 
             className="content"
             style={{
