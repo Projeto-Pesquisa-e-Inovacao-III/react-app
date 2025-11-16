e7e25caf065fe7ef852f14bc670ebc1db46d06e3
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-11-16 01:33:20 -0300

    feat: Enhance AddPackagePlan component with new fields and API integration; update related components and types for better package management

---
+++ src/models/products.ts
@@ -0,0 +1,11 @@
+export type ProductExhibition = {
+    "titulo": string,
+    "subtitulo": string,
+    "descricao": string,
+    "preco": string,
+    "periodo": string,
+    "status": string,
+    "tipoAula": string,
+    "quantidadeAula": number,
+    "duracaoMes": number
+}
