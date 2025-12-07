465a93731c2efd1e0864d1723ce7106856711f09
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-12-07 19:19:57 -0300

    fix: implement package fetching and dynamic rendering in PlansSection

--- src/routes/Home/PlansSection/index.tsx
+++ src/routes/Home/PlansSection/index.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import PlansCard from "../../../components/Home/PlansCard";
+import { useQuery } from "@tanstack/react-query";
+import { getPackages } from "../../../constants/home";
 
 export default function PlansSection({ isMobile }: { isMobile: boolean }) {
     const [isPackagesSelected, setIsPackagesSelected] = useState(true);
+
+    const packages = useQuery({
+        queryKey: ['packages'],
+        queryFn: getPackages,
+        select: (res) => res.data,
+    });
+
+    console.log(packages.data);
 
     return (
         <section id="plans-section" className={`bg-indigo p-5 pt-10 pb-10 ${isMobile ? "mt-10" : ""}`}>
@@ -14,18 +24,26 @@ export default function PlansSection({ isMobile }: { isMobile: boolean }) {
                 </div>
                 <div className={`${isMobile ? "grid grid-cols-1" : "grid grid-cols-[repeat(auto-fit,minmax(0,1fr))] "} gap-8 h-full`}>
                     {isPackagesSelected ? (
-                        <>
-                            <PlansCard description="12 meses" content="Pacote Anual" price="R$ 2.400,00" benefits={["Treino personalizado;", "Anamnese;", "Feedback Diário;", "1 aula grátis com o personal a cada mês   (12 aulas).", "Dicas de Suplementação.", "Indicação de Nutricionistas."]} />
-                            <PlansCard description="6 meses" content="Pacote Semestral" price="R$ 700,00" benefits={["Treino personalizado;", "Anamnese;", "Feedback Diário;", "1 aula grátis com o personal a cada mês   (6 aulas).", "Dicas de Suplementação."]} />
-                            <PlansCard description="3 meses" content="Pacote Trimestral" price="R$ 500,00" benefits={["Treino personalizado;", "Anamnese;", "Feedback Diário;", "1 aula grátis com o personal a cada mês   (3 aulas)."]} />
-                            <PlansCard description="1 mês" content="Pacote Mensal" price="R$ 200,00" benefits={["Treino personalizado;", "Anamnese;", "Feedback Diário;"]} />
-                        </>
+                        packages.data?.filter((pkg: any) => pkg.tipoProduto === "PACOTE").map((pkg: any) => (
+                            <PlansCard
+                                key={pkg.id}
+                                description={pkg.periodo}
+                                content={pkg.titulo}
+                                price={`R$ ${pkg.preco}`}
+                                benefits={JSON.parse(pkg.descricao)}
+                            />
+                        ))
+
                     ) : (
-                        <>
-                            <PlansCard description="As aulas com personal são cobradas separadamente da consultoria." content="Personal Residencial" price="R$ 200" benefits={["Atendimento personalizado em sua residência, com duração de 1 hora por aula."]} />
-                            <PlansCard description="As aulas com personal são cobradas separadamente da consultoria." content="Aulas com Personal" price="R$ 120" benefits={["Treinos individuais com personal na academia, com duração de 1 hora a aula."]} />
-                            <PlansCard description="As aulas com personal são cobradas separadamente da consultoria." content="Aula Funcional" price="R$ 100" benefits={["Treinos coletivos focados em condicionamento físico e resistência com 30 minutos de aula."]} />
-                        </>
+                        packages.data?.filter((pkg: any) => pkg.tipoProduto === "ADICIONAL").map((pkg: any) => (
+                            <PlansCard
+                                key={pkg.id}
+                                description={pkg.periodo}
+                                content={pkg.titulo}
+                                price={`R$ ${pkg.preco}`}
+                                benefits={JSON.parse(pkg.descricao)}
+                            />
+                        ))
                     )}
                 </div>
             </div>
