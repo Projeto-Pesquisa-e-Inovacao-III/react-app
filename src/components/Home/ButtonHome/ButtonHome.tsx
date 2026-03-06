3568c756097d58959daf64cdc54d381ac3099f7c
andradeGabriel05 <g.andrade0222@gmail.com>
2026-03-06 19:36:24 -0300

    feat: update AboutSection to use new about image and adjust layout for mobile

--- src/components/Home/ButtonHome/ButtonHome.tsx
+++ src/components/Home/ButtonHome/ButtonHome.tsx
@@ -1,6 +1,6 @@
 export default function ButtonHome({ title, to }: { title: string, to: string }) {
     return (
-        <a href={to} className="flex justify-center items-center bg-white min-h-12 w-[53.2%] mt-10 text-black font-semibold rounded-md cursor-pointer">
+        <a href={to} className="flex text-center justify-center items-center bg-white min-h-12 w-[53.2%] mt-10 text-black font-semibold rounded-md cursor-pointer">
             {title}
         </a>
     );
