c07e9eee893a3f25a88e405e8e80af1d5d1e8817
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-04-12 21:33:41 -0300

    add - env url config

--- src/system.ts
+++ src/system.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:8080/api"
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8080/api"
 
 export const api = axios.create({
     baseURL: BASE_URL,
