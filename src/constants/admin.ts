eaf46f8ad29dd6168d062d73ae550663501a2a51
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-04-19 19:46:33 -0300

    add: admin dtos and functions

---
+++ src/constants/admin.ts
@@ -0,0 +1,26 @@
+import type { ReqAdicionarRoleDTO, ResCadastrarPersonalDTO, ResNeedDataDTO, ResUsuarioWithRolesResponseDTO } from "../models/admin";
+import { api } from "../system";
+
+export function getUsers(): Promise<ResUsuarioWithRolesResponseDTO[]> {
+    return api.get("/admin/usuarios");
+}
+
+export function deleteUser(id: number) {
+    return api.put(`/admin/usuarios/${id}/deletar`);
+}
+
+export function getVerifyNeedDataToAddRole(id: number, role: string): Promise<ResNeedDataDTO> {
+    return api.get(`/admin/usuarios/${id}/roles/perfil?role=${role}`);
+}
+
+export function addRoleToUser(id: number, role: string, extraData?: ReqAdicionarRoleDTO) {
+    return api.put(`/admin/usuarios/${id}/roles?role=${role}`, { data: extraData });
+}
+
+export function removeRoleFromUser(id: number, role: string) {
+    return api.delete(`/admin/usuarios/${id}/roles?role=${role}`);
+}
+
+export function createPersonal(data: ResCadastrarPersonalDTO): Promise<ResCadastrarPersonalDTO> {
+    return api.post("/admin/usuarios/personal", data);
+}
